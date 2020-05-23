9c89f7b87f89d8c6ad745fc408421f7861853f7c
danielTiringer <tiringerdaniel@gmail.com>
2020-05-23 17:53:38 +0200

    Install MongoDB and put connection in place.

--- node/app.js
+++ node/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const PORT = 8080;
 
 const app = express();
+
+dotenv.config();
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
@@ -13,6 +17,10 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+mongoose.connect(process.env.MONGO_URI, () => {
+    console.log(`Connected to MongoDB.`);
+})
+
 app.listen(PORT, () => {
-    console.log(`Server listening to requests on port ${PORT}`);
+    console.log(`Server listening to requests on port ${PORT}.`);
 })
