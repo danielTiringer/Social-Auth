bf9b6bf179f566a89acd54c7b784a0801f02a93f
danielTiringer <tiringerdaniel@gmail.com>
2020-05-23 08:14:45 +0200

    Add google keys to strategy

--- node/config/passport-setup.js
+++ node/config/passport-setup.js
@@ -1,10 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const dotenv = require('dotenv');
+dotenv.config();
 
 passport.use(
     new GoogleStrategy({
-
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }), () => {
-
+       
     }
 );
