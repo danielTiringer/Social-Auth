dce12ef1a879bbf273829ce28eca8de4a55b5295
danielTiringer <tiringerdaniel@gmail.com>
2020-05-23 08:27:41 +0200

    Add google redirect address

--- node/config/passport-setup.js
+++ node/config/passport-setup.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 passport.use(
     new GoogleStrategy({
+        callbackURL: '/auth/google/redirect',
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }), () => {
