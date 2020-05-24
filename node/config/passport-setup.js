37b2943af02ac141218b51569c0a90395829e887
danielTiringer <tiringerdaniel@gmail.com>
2020-05-24 09:50:43 +0200

    Check if user exits

--- node/config/passport-setup.js
+++ node/config/passport-setup.js
@@ -12,13 +12,27 @@ passport.use(
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET
     }, (accessToken, refreshToken, profile, done) => {
-        new User({
-            username: profile.displayName,
-            googleId: profile.id
-        })
-            .save()
-            .then(newUser => {
-                console.log(`New user created: ${newUser}`);
+        User.findOne({ googleId: profile.id })
+            .then((currentUser) => {
+                if (currentUser) {
+                    console.log(`The user is: ${currentUser}`);
+                } else {
+                    new User({
+                        username: profile.displayName,
+                        googleId: profile.id
+                    })
+                        .save()
+                        .then(newUser => {
+                            console.log(`New user created: ${newUser}`);
+                        })
+                        .catch(err => {
+                            if (err) throw err;
+                        })
+                }
             })
+            .catch(err => {
+                if (err) throw err;
+            })
+
     })
 );
