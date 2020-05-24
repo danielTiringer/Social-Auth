c5064ed031e93ab23b557370d648c5660352bcac
danielTiringer <tiringerdaniel@gmail.com>
2020-05-24 16:49:22 +0200

    Implement proper profile view

--- Node/node/routes/profile-routes.js
+++ Node/node/routes/profile-routes.js
@@ -9,7 +9,7 @@ const authCheck = (req, res, next) => {
 };
 
 router.get('/', authCheck, (req, res) => {
-    res.send(`You are logged in, your profile has the following: ${req.user.username}`);
+    res.render('profile', { user: req.user });
 });
 
 module.exports = router;
