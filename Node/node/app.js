7f85c6aa7aeb474b89c1738489a1ce3c52cae1e2
danielTiringer <tiringerdaniel@gmail.com>
2020-05-24 16:03:12 +0200

    Add cookie-session and initialize in app

--- Node/node/app.js
+++ Node/node/app.js
@@ -3,13 +3,25 @@ const authRoutes = require('./routes/auth-routes');
 const passportSetup = require('./config/passport-setup');
 const db = require('./config/database');
 const dotenv = require('dotenv');
-const PORT = 8080;
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 const app = express();
 
 dotenv.config();
 
+const PORT = process.env.PORT || 8080;
+
 app.set('view engine', 'ejs');
+
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000, // in milliseconds
+    keys: [process.env.ENCRYPTION_KEY]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(__dirname));
 app.use('/auth', authRoutes);
 
