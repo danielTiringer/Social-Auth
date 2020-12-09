30b720aa6326c9f30b581c2fb570ae007fc99f54
Daniel Tiringer <tiringerdaniel@gmail.com>
2020-12-09 20:29:57 +0100

    Add login and register functionality

--- Python/flask/__init__.py
+++ Python/flask/__init__.py
@@ -8,10 +8,20 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = 'superdupersecretkey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:password@mysql/db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@db/db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
