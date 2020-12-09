60238299ba63685c49bbb6ea6e0c5db678f1b47f
Daniel Tiringer <tiringerdaniel@gmail.com>
2020-12-09 21:45:48 +0100

    Add github oauth login basic setup

--- Python/flask/__init__.py
+++ Python/flask/__init__.py
@@ -1,6 +1,8 @@
-from flask import Flask
+import os
+from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_dance.contrib.github import make_github_blueprint, github
 
 db = SQLAlchemy()
 
@@ -28,5 +30,26 @@ def create_app():
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    github_blueprint = make_github_blueprint(
+        client_id=os.environ.get('GITHUB_OAUTH_CLIENT_ID'),
+        client_secret=os.environ.get('GITHUB_OAUTH_CLIENT_SECRET')
+    )
+    app.register_blueprint(github_blueprint, url_prefix='/github_login')
+
+    @app.route('/github')
+    def github_login():
+        if not github.authorized:
+            return redirect(url_for('github.login'))
+
+        account_info = github.get('/user')
+
+        if account_info.ok:
+            account_info_json = account_info.json()
+            return "You are @{login} on Github".format(login=account_info.json()["login"])
+
+            return redirect(url_for('main.profile'))
+
+        return redirect(url_for('auth.login'))
 
     return app
